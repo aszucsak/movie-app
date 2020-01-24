0ea18a97c6d840def7f512e9c5543cb3a395d317
Andras <aszucsak@gmail.com>
2020-01-24 21:50:23 +0100

    add Actors grid

--- src/components/elements/Actor.js
+++ src/components/elements/Actor.js
@@ -1,7 +1,26 @@
 import React from "react";
 
-function Actor() {
-  return <div>Actor</div>;
+import NoImage from "../images/no_image.jpg";
+
+import { IMAGE_BASE_URL, POSTER_SIZE } from "../../config";
+
+import { StyledActor } from "../styles/StyledActor";
+
+function Actor({ actor }) {
+  return (
+    <StyledActor>
+      <img
+        src={
+          actor.profile_path
+            ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+            : NoImage
+        }
+        alt="actorthumb"
+      />
+      <span className="actor-name">{actor.name}</span>
+      <span className="actor-character">{actor.character}</span>
+    </StyledActor>
+  );
 }
 
 export default Actor;
