7ae3dcc44990d2e0d44e687e058aa0b8a30fb74d
Andras <aszucsak@gmail.com>
2020-01-24 18:06:28 +0100

    create elements for Movie page

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,7 +1,24 @@
 import React from "react";
 
+import Navigation from "./elements/Navigation";
+import MovieInfo from "./elements/MovieInfo";
+import MovieInfoBar from "./elements/MovieInfoBar";
+import Actor from "./elements/Actor";
+import Grid from "./elements/Grid";
+import Spinner from "./elements/Spinner";
+
 function Movie({ movieId }) {
-  return <div>Movie: {movieId}</div>;
+  return (
+    <>
+      <Navigation />
+      <MovieInfo />
+      <MovieInfoBar />
+      <Grid>
+        <Actor />
+      </Grid>
+      <Spinner />
+    </>
+  );
 }
 
 export default Movie;
